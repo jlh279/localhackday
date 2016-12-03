92a72a200763b7893520863f02549d068bd20d26
jlh279 <clairesbirthdaypresent@gmail.com>
2016-12-03 11:36:54 -0500

    Adding storage

--- js/popup.js
+++ js/popup.js
@@ -11,10 +11,36 @@ function startGame() {
 		var url = "https://en.wikipedia.org/wiki/Special:Random";
 	else
 		var url = "https://en.wikipedia.org/wiki/" + startPage;
-	
+
 	// Create new tab
 	chrome.tabs.create({"url": url}, function(tab) {
 		tabId = tab.id;
 	});
 	chrome.tabs.update(tabId, {url: url});
 }
+
+function saveChanges() {
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+    });
+
+    chrome.storage.sync.get("urls")
+    // Save it using the Chrome extension storage API.
+    chrome.storage.sync.set({'value': theValue}, function() {
+      // Notify that we saved.
+      message('Settings saved');
+    });
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+});
